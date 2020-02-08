fc7f64100908f0d6af7d1185816de157c4fa7f59
Sr_developer <sreekala.anirudhan@northportdata.com>
2020-02-08 20:45:28 +0530

    First

---
+++ App.js
@@ -0,0 +1,38 @@
+// Import from 'react-native-pdf-lib'
+import PDFLib, { PDFDocument, PDFPage } from 'react-native-pdf-lib';
+ 
+// Create a PDF page with text and rectangles
+const page1 = PDFPage
+  .create()
+  .setMediaBox(200, 200)
+  .drawText('You can add text and rectangles to the PDF!', {
+    x: 5,
+    y: 235,
+    color: '#007386',
+  })
+  .drawRectangle({
+    x: 25,
+    y: 25,
+    width: 150,
+    height: 150,
+    color: '#FF99CC',
+  })
+  .drawRectangle({
+    x: 75,
+    y: 75,
+    width: 50,
+    height: 50,
+    color: '#99FFCC',
+  });
+  
+// Create a new PDF in your app's private Documents directory
+const docsDir = await PDFLib.getDocumentsDirectory();
+const pdfPath = `${docsDir}/sample.pdf`;
+PDFDocument
+  .create(pdfPath)
+  .addPages(page1)
+  .write() // Returns a promise that resolves with the PDF's path
+  .then(path => {
+    console.log('PDF created at: ' + path);
+    // Do stuff with your shiny new PDF!
+  });
